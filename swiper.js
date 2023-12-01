fd95b4dc37708e85dd4fb2f343de0e55885d1cdb
Anna Muzalevskaya <77303782+NitaLewska@users.noreply.github.com>
2023-12-01 20:54:19 +0300

    fix: add swiper breakpoints; feat: add favicon

--- swiper.js
+++ swiper.js
@@ -9,10 +9,22 @@ var swiper = new Swiper(".swiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  slidesPerView: 3,
   loop: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    740: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 40
+    }
+  }
 });
